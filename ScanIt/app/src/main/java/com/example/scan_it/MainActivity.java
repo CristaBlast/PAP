77d4cf1901f90afdf7f24073e1b6a425769912e6
Cristiano <cristianoben10@gmail.com>
2021-05-17 23:48:35 +0100

    Inicial App

---
+++ ScanIt/app/src/main/java/com/example/scan_it/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.scan_it;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+    String currentPhotoPath;
+    Bitmap sbmp;
+    String testText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frameLayout = (FrameLayout)findViewById(R.id.cameraPreview);
+        camera = Camera.open();
+        showCamera = new ShowCamera(this,camera);
+        frameLayout.addView(showCamera);
+        //Important bmp = BitmapFactory.decodeResource(getResources(),)//path)
+    }
+
+    //PDF
+    public void pfd(View v)
+    {
+        String pdfName;
+        EditText pdfN = (EditText) findViewById(R.id.textBoxPDFName);
+        pdfName = pdfN.getText().toString();
+        PdfDocument pdf = new PdfDocument();
+        Paint paint = new Paint();
+
+
+
+
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(400,600,1).create();
+        PdfDocument.Page page = pdf.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+        canvas.drawBitmap(sbmp,400,600,paint);
+
+
+
+
+        File file = new File(Environment.getExternalStorageDirectory(),"/" + pdfName + ".pdf");
+
+
+
+        
+        pdf.finishPage(page);
+        pdf.close();
+     }
+
+     //End PDF
+
+
+    //Camera
+    public void capturePhoto(View v)
+    {
+        Button btnF = findViewById(R.id.btnFinish);
+
+        if(camera!=null)
+        {
+            camera.takePicture(null,null,mPictureCallback);
+//            Toast.makeText(getApplicationContext(),"Stuff",Toast.LENGTH_SHORT).show();
+        }
+        camera.stopPreview();
+        camera.startPreview();
+
+
+        btnF.setVisibility(View.VISIBLE);
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback()
+    {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOutputMediaFile(1);
+            if(picture_file == null)
+            {
+                return;
+            }
+            else
+            {
+                try
+                {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+
+                    camera.startPreview();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    /** Create a File for saving an image or video **/
+    public static File getOutputMediaFile(int type)
+    {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "ScanIt");
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        // Create the storage directory if it does not exist
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("ScanIt", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_"+ timeStamp + ".jpg");
+
+
+
+        }
+        else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+//
+//    public void galleryAddPic() {
+//        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//        File f = new File(currentPhotoPath);
+//        Uri contentUri = Uri.fromFile(f);
+//        mediaScanIntent.setData(contentUri);
+//        this.sendBroadcast(mediaScanIntent);
+//    }
+    //End Camera
+
+    public void goTWelcomePage(View v)
+    {
+        setContentView(R.layout.welcome_page);
+    }
+
+    public void goTMainPage(View v)
+    {
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goTSavePage(View v)
+    {
+        setContentView(R.layout.save_page);
+        TextView tvt = (TextView)findViewById(R.id.textTestPage);
+        tvt.setText(testText);
+    }
+}
