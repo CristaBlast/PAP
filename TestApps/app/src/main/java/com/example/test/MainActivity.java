59d2bb67ab3a9ebf4c94060c1ae3a7c3be36da01
CristaBlast <cristianoben10@gmail.com>
2021-05-18 09:42:31 +0100

    Added Test Spot

---
+++ TestApps/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.test;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.graphics.drawable.GradientDrawable;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextureView textureView;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
+    private String cameraId;
+    CameraDevice cameraDevice;
+    CameraCaptureSession cameraCaptureSession;
+    CaptureRequest captureRequest;
+    CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimensions;
+    private ImageReader imageReader;
+    private File file;
+    Handler mBackgroundHandler;
+    HandlerThread mBackgroundTread;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textureView = (TextureView) findViewById(R.id.texView);
+        btn = (Button) findViewById(R.id.btnPhoto);
+        textureView.setSurfaceTextureListener(textureListener);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+                {
+                    takePicture();
+                }
+        });
+
+    }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+//            try {
+//                openCamera();
+//            }
+//            catch (CameraAccessException e)
+//            {
+//                e.printStackTrace();
+//            }
+
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+
+        }
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void openCamera() {
+        CameraManager manager = (CameraManager)getSystemService((Context.CAMERA_SERVICE));
+         cameraId = manager.getCameraIdList()[0];
+        CameraCharacteristics characteristics = manager.getCameraCharacteristics((cameraId));
+        StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        imageDimensions = map.getOutputSizes(SurfaceTexture.class)[0];
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},101);
+            return;
+        }
+        manager.openCamera(cameraId,stateCallback,null);
+
+    }
+
+    private void takePicture() {
+
+    }
+
+    public void clicked(View v)
+    {
+//        Toast.makeText(getApplicationContext(),"Btn clicked",Toast.LENGTH_SHORT).show();
+        File file = new File(Environment.getExternalStorageDirectory() + "/177013");
+        if(!file.mkdir())
+        {
+            file.mkdir();
+            Toast.makeText(getApplicationContext(),"Folder Created",Toast.LENGTH_SHORT).show();
+        }
+        String filepath = file.getAbsolutePath();
+        TextView tvt = (TextView) findViewById(R.id.testText);
+        tvt.setText(filepath);
+//        Toast.makeText(getApplicationContext(),"Folder Path",Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void nextP (View v)
+    {
+        setContentView(R.layout.camera);
+    }
+}
