3ff33cfab23bf513a0c1975dfbb4f3e346dd6aa5
CristaBlast <cristianoben10@gmail.com>
2021-05-21 16:04:58 +0100

    Doing stuff/ Created folder Root for photos

--- ScanIt/app/src/main/java/com/example/scan_it/MainActivity.java
+++ ScanIt/app/src/main/java/com/example/scan_it/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.scan_it;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -86,7 +89,40 @@ public class MainActivity extends AppCompatActivity {
     public void capturePhoto(View v)
     {
         Button btnF = findViewById(R.id.btnFinish);
-
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()))
+        {
+            Toast.makeText(getApplicationContext(), "Has Storage Exists", Toast.LENGTH_SHORT).show();
+            if (checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            {
+                File file = new File(Environment.getExternalStorageDirectory() + "/Scan-It");
+                if(!file.mkdir())
+                {
+                    file.mkdir();
+                    Toast.makeText(getApplicationContext(),"Root Folder Created",Toast.LENGTH_SHORT).show();
+                }
+//                String text = "stuff here :3";
+//                Toast.makeText(getApplicationContext(), file.toString(), Toast.LENGTH_SHORT).show();
+//                File textFile = new File(file,FILE_NAME);
+//                Toast.makeText(getApplicationContext(), textFile.toString(), Toast.LENGTH_SHORT).show();
+//                try {
+//                    FileOutputStream fos = new FileOutputStream(textFile);
+//                    fos.write(text.getBytes());
+//                    fos.close();
+//                    Toast.makeText(getApplicationContext(), "Photo Saved", Toast.LENGTH_SHORT).show();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(getApplicationContext(), "Cannot Save Photo", Toast.LENGTH_SHORT).show();
+//                }
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Can't write on external storage", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Doesn't have external storage", Toast.LENGTH_SHORT).show();
+        }
         if(camera!=null)
         {
             camera.takePicture(null,null,mPictureCallback);
@@ -94,6 +130,15 @@ public class MainActivity extends AppCompatActivity {
         }
         camera.stopPreview();
         camera.startPreview();
+
+
+//        if(camera!=null)
+//        {
+//            camera.takePicture(null,null,mPictureCallback);
+//            Toast.makeText(getApplicationContext(),"Shutter clicked",Toast.LENGTH_SHORT).show();
+//        }
+//        camera.stopPreview();
+//        camera.startPreview();
 
 
         btnF.setVisibility(View.VISIBLE);
@@ -115,7 +160,9 @@ public class MainActivity extends AppCompatActivity {
                     FileOutputStream fos = new FileOutputStream(picture_file);
                     fos.write(data);
                     fos.close();
+
                     Toast.makeText(getApplicationContext(), "Photo saved II", Toast.LENGTH_SHORT).show();
+
                     camera.startPreview();
                 }
                 catch (IOException e)
@@ -129,26 +176,30 @@ public class MainActivity extends AppCompatActivity {
     private File getOutputMediaFile()
     {
         String state = Environment.getExternalStorageState();
-        if(!state.equals(Environment.MEDIA_MOUNTED))
-        {
-            return null;
-        }
-        else
-        {
+//        if(!state.equals(Environment.MEDIA_MOUNTED))
+//        {
+//            return null;
+//        }
+//        else
+//        {
             File pathFolder = new File(Environment.getExternalStorageDirectory() + "/Scan-It");
             if(!pathFolder.exists())
             {
                 pathFolder.mkdir();
-                Toast.makeText(getApplicationContext(), "Folder Created ", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Folder Created", Toast.LENGTH_SHORT).show();
             }
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             File outputFile = new File(pathFolder,"IMG_" + timeStamp);
-            Toast.makeText(getApplicationContext(), "Photo Created I", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Photo Created", Toast.LENGTH_SHORT).show();
             return outputFile;
-        }
+//        }
     }
 
-
+    public boolean checkPermission(String permission)
+    {
+        int check = ContextCompat.checkSelfPermission(this,permission);
+        return (check == PackageManager.PERMISSION_GRANTED);
+    }
 
 
 
